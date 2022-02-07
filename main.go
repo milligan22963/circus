6a0554e37d1e7078ae34de1a3a4cadbde3a4eaa9
Dan Milligan <dwm@afmsoftware.com>
2022-02-06 20:15:58 -0500

    clean up

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/milligan22963/circus/cmd"
-	local "github.com/milligan22963/circus/pkg/management"
 
 	"github.com/spf13/cobra"
-	"tinygo.org/x/bluetooth"
 )
-
-var adapter = bluetooth.DefaultAdapter
 
 func main() {
 	rootCmd := &cobra.Command{
@@ -33,51 +24,5 @@ func main() {
 
 	if err := rootCmd.Execute(); err != nil {
 		println("failed to initialize: ", err.Error())
-	}
-
-	err := adapter.Enable()
-
-	if err != nil {
-		fmt.Printf("failed to enable adapter: %+v", err)
-		return
-	}
-
-	targetName := "Dark Circus"
-	ch := make(chan bluetooth.ScanResult, 1)
-
-	err = adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
-		fmt.Printf("%+v, payload: %+v", result, result.AdvertisementPayload)
-		//		println("found device:", result.Address.String(), result.RSSI, result.LocalName())
-		if result.LocalName() == targetName {
-			adapter.StopScan()
-			ch <- result
-		}
-	})
-
-	if err != nil {
-		fmt.Printf("failed to enable scan: %+v", err)
-		return
-	}
-
-	result := <-ch
-
-	var skull local.Skull
-	err = skull.Connect(adapter, result.Address)
-	if err != nil {
-		println(err.Error())
-		return
-	}
-
-	println("connected to ", result.Address.String())
-
-	signals := make(chan os.Signal, 1)
-
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-
-	<-signals
-
-	err = skull.Disconnect()
-	if err != nil {
-		println(err)
 	}
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -8,7 +8,9 @@ import (
 	"syscall"
 
 	"github.com/milligan22963/circus/config"
+	"github.com/milligan22963/circus/pkg/management"
 	"github.com/milligan22963/circus/pkg/web"
+	"tinygo.org/x/bluetooth"
 )
 
 // HTTPResponse is a structure defining what a response should look like
@@ -20,6 +22,7 @@ type HTTPResponse struct {
 // ServerInstance is an instance of server
 type ServerInstance struct {
 	ServerPort int
+	Skull      *management.Skull
 }
 
 func (server *ServerInstance) waitForExit() {
@@ -37,7 +40,52 @@ func (server *ServerInstance) waitForExit() {
 	<-doneFlag
 }
 
+var adapter = bluetooth.DefaultAdapter
+
+func (server *ServerInstance) SetupSkull() (*management.Skull, error) {
+	err := adapter.Enable()
+
+	if err != nil {
+		return nil, err
+	}
+
+	targetName := "Dark Circus"
+	ch := make(chan bluetooth.ScanResult, 1)
+
+	err = adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
+		fmt.Printf("%+v, payload: %+v", result, result.AdvertisementPayload)
+		if result.LocalName() == targetName {
+			adapter.StopScan()
+			ch <- result
+		}
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := <-ch
+
+	skull := &management.Skull{}
+	err = skull.Connect(adapter, result.Address)
+	if err != nil {
+		return nil, err
+	}
+	return skull, nil
+}
+
 func (server *ServerInstance) Run(appConfig *config.AppConfiguration) {
+	defer func() {
+		server.Skull.Disconnect()
+	}()
+
+	var err error
+	server.Skull, err = server.SetupSkull()
+
+	if err != nil {
+		return
+	}
+
 	webServer := web.WebServer{}
 
 	// server up the world
